8bf7db5344d3c0a1f4f1d5d0f9c5ab4ec64cdb52
stepansolovev <spqtoo@ya.ru>
2022-10-27 16:00:34 +0300

    Initial Commit

---
+++ HTTPRequester/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HTTPRequester
+//
+//  Created by Степан Соловьёв on 27.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
